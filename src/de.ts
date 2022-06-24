4d444eb67f696be80547255e3500c913c53dce3a
Wavelopper <alnetweb59@gmail.com>
2022-06-24 09:23:42 +0200

    [INIT] initalisation of project

---
+++ src/de.ts
@@ -0,0 +1,13 @@
+export class De{
+    private _dice:number = 0;
+
+
+    get_valeur(){
+        return this._dice;
+    }
+
+    lancer(value: De){
+        let randomDice = Math.floor(6*Math.random())+1;
+          
+    }
+}
